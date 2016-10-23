ece58457d31dc0009ed39becfece1c614a494835
Feroze Naina <ferozenaina@gmail.com>
2016-10-23 15:41:49 -0400

    Added visualization inside map.h. Removed map_parser.h and replaced with one unified class for map and visualization

---
+++ src/map.cpp
@@ -0,0 +1,101 @@
+#include <map.h>
+#include <particle_state.h>
+
+
+using namespace cv;
+using namespace ps;
+
+Map::Map(std::string filename)
+{	
+
+  // Defining certain constants instead of reading from file
+  size_x = 800;
+  size_y = 800;
+  res = 10;
+
+  // initialize
+  grid = Mat::zeros(size_x, size_y, CV_64FC1);
+
+  // parse the map from dat file
+  readMap(filename);
+}
+
+void Map::readMap(std::string file){
+
+  std::ifstream fin(file);
+
+  if (! fin.good())
+  {
+    printf("Bad File Path!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  std::string line, ch;
+  int count = 0;
+  double val;
+
+  while (std::getline(fin, line)){
+
+    count++;
+
+    // skip first 7 lines
+    if(count == 7)
+      break;
+  }
+
+  count = 0;
+  while ( fin >> val){
+    int temprow = count / size_x;
+    int tempcol = count % size_x;
+    grid.at<double>(temprow, tempcol) = val;
+    //printf(" %f ", val);
+    count++;
+  }
+
+}
+
+void Map::printMap(){
+
+  cv::Size s = grid.size();
+
+  printf("\nSize x : %d\n", size_x);
+  printf("\nSize y : %d\n", size_y);
+  printf("\nResolution : %d\n", res);
+  printf("\nMap size : %d\n", grid_size);
+
+  printf("\nMap : \n");
+
+  for(int i = 0; i < s.height; i++){
+    for(int j = 0; j < s.width; j++){
+
+      std::cout << grid.at<double>(i,j) << " " ;
+    }
+    std::cout << std::endl;
+  }
+}
+
+void Map::displayMap(){
+
+  namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+  imshow( "Display window", grid);                   // Show our image inside it.
+
+  waitKey(0);                                          // Wait for a keystroke in the window
+}
+
+void Map::visualizeParticles(vector<ParticleState>* particle_list) {
+
+  Mat temp_grid = grid;
+  Point pt;
+  cv::Scalar red(255, 0, 0);
+
+  for(std::vector<ParticleState>::iterator it = particle_list->begin(); it != particle_list->end(); ++it) {
+    pt = Point(int(it->x()/res), int(it->y()/res)); // divided by res as one pixel in visualization = 10 units of distance
+    circle(temp_grid, pt, 2, red);
+  }
+
+  namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+  imshow( "Display window", temp_grid);                   // Show our image inside it.
+
+  waitKey(0);                                          // Wait for a keystroke in the window
+  destroyWindow( "Display window");
+}
--- src/particle_state.cpp
+++ src/particle_state.cpp
@@ -4,82 +4,82 @@ using namespace rs;
 
 namespace ps{
 
-ParticleState::ParticleState(RobotState robot_state, double weight)
-:
-x_(robot_state.x()),
-y_(robot_state.y()),
-theta_(robot_state.theta()),
-robot_state_(robot_state),
-weight_(weight)
-{
-	utils_ = new utils::UtilFunctions();
-}
+  ParticleState::ParticleState(RobotState robot_state, double weight)
+    :
+      x_(robot_state.x()),
+      y_(robot_state.y()),
+      theta_(robot_state.theta()),
+      robot_state_(robot_state),
+      weight_(weight)
+  {
+    utils_ = new utils::UtilFunctions();
+  }
 
-ParticleState::ParticleState(double x, double y, double theta, double weight)
-:
-x_(x),
-y_(y),
-theta_(theta),
-weight_(weight)
-{
-	utils_ = new utils::UtilFunctions();
+  ParticleState::ParticleState(double x, double y, double theta, double weight)
+    :
+      x_(x),
+      y_(y),
+      theta_(theta),
+      weight_(weight)
+  {
+    utils_ = new utils::UtilFunctions();
 
-	robot_state_.x(x_);
-	robot_state_.y(y_);
-	robot_state_.theta(theta_);
-}
+    robot_state_.x(x_);
+    robot_state_.y(y_);
+    robot_state_.theta(theta_);
+  }
 
-void ParticleState::x(double x){
-	x_ = x;
-}
+  void ParticleState::x(double x){
+    x_ = x;
+  }
 
-void ParticleState::y(double y){
-	y_ = y;
-}
+  void ParticleState::y(double y){
+    y_ = y;
+  }
 
-void ParticleState::theta(double theta){
-	theta_ = theta;
-}
+  void ParticleState::theta(double theta){
+    theta_ = theta;
+  }
 
-void ParticleState::robot_state(RobotState robot_state){
-	robot_state_ = robot_state;
-	x_ = robot_state.x();
-	y_ = robot_state.y();
-	theta_ = robot_state.theta();
-}
+  void ParticleState::robot_state(RobotState robot_state){
+    robot_state_ = robot_state;
+    x_ = robot_state.x();
+    y_ = robot_state.y();
+    theta_ = robot_state.theta();
+  }
 
-void ParticleState::weight(double weight){
-	weight_ = weight;
-}
+  void ParticleState::weight(double weight){
+    weight_ = weight;
+  }
 
-double ParticleState::x(){
-	return x_;
-}
+  double ParticleState::x(){
+    return x_;
+  }
 
-double ParticleState::y(){
-	return y_;
-}
+  double ParticleState::y(){
+    return y_;
+  }
 
-double ParticleState::theta(){
-	return theta_;
-}
+  double ParticleState::theta(){
+    return theta_;
+  }
 
-RobotState ParticleState::robot_state(){
-	return robot_state_;
-}
+  RobotState ParticleState::robot_state(){
+    return robot_state_;
+  }
 
-double ParticleState::weight(){
-	return weight_;
-}
+  double ParticleState::weight(){
+    return weight_;
+  }
 
-ParticleState ParticleState::rotate(double theta){
+  ParticleState ParticleState::rotate(double theta){
 
-	ParticleState trans_state;
-	trans_state.robot_state(robot_state_.rotate(theta));
-	trans_state.weight(weight_);
+    ParticleState trans_state;
+    trans_state.robot_state(robot_state_.rotate(theta));
+    trans_state.weight(weight_);
 
-	return trans_state;	
+    return trans_state;	
 
-}
+  }
 
 } //namepsace
