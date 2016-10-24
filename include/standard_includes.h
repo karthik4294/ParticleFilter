f10ef9c97a901e013fd262fec93de46aa7ff7b24
sameerbardapurkar <sbardapu@andrew.cmu.edu>
2016-10-24 00:59:10 -0400

    added file reader

--- include/standard_includes.h
+++ include/standard_includes.h
@@ -17,11 +17,8 @@
 #include <algorithm>
 #include <ctime>
 #include <unordered_map>
-<<<<<<< HEAD
 #include <map>
-=======
 #include <random>
->>>>>>> c3652abf67cabe947bbe456053d4217c271591d6
 #include <cmath>
 
 #endif
